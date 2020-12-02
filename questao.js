9ad638acd2500759ca8bedbc0d40f23da0cf3055
diogoporto <61921306+diogoporto@users.noreply.github.com>
2020-12-02 19:50:26 -0300

    Add files via upload

---
+++ questao.js
@@ -0,0 +1,55 @@
+
+
+// questões
+const quiz =[
+    {
+        q:'Quantas vezes o brasil ganhou a Copa do Mundo de Futebol?',
+        opcao:['03','05','06','07'],
+        answer:1
+    },
+    {
+        q:'Quantas Copas do Mundo de Futebol já foram realizadas até hoje?',
+        opcao:['14','10','18','20'],
+        answer:3
+    },
+    {
+        q:'Quantas seleções conseguiram vencer uma Copa do Mundo de Futebol?',
+        opcao:['05','13','08','10'],
+        answer:2
+    },
+    {
+        q:'Qual o jogador Brasileiro fez mais Gols em Copas do Mundo?',
+        opcao:['Ronaldo','Romário','Pelé','Neynar'],
+        answer:0
+    },
+    {
+            q:'Em que ano foi realizada a primeira Copa do Mundo?',
+        opcao:['1934','1930','1938','1942'],
+        answer:1
+    },
+    {
+        q:'Que país ganhou a primeira Copa do Mundo?',
+        opcao:['Paraguai','Argentina','Brasil','Uruguai'],
+        answer:3
+    },
+    {
+        q:'Quem criou a Copa do Mundo?',
+        opcao:['Jule Rimet','Claudio Adão','Jules Bellini','Jeans Rimet'],
+        answer:0
+    },
+    {
+        q:'Qual país sediou a Copa do Munde de 1994?',
+        opcao:['Rússia','México','Estados Unidos','Alemanha'],
+        answer:2
+    },
+    {
+        q:'Onde será realizada a Copa do Mundo de 2022?',
+        opcao:['Alemanha','Catar','Argentina','Angola'],
+        answer:1
+    },
+    {
+        q:'Qual o maior artilheiro em jogos oficial pelo Brasil?',
+        opcao:['Ronaldo','Romário','Pelé','Neymar'],
+        answer:2
+    }
+]
